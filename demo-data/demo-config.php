9616465531d6378ed393118959ce3dd2c4151ba9
Md. Lutful Haque Pappana <lutfulpappana71@gmail.com>
2025-11-10 16:29:32 +0600

    Add files via upload

---
+++ demo-data/demo-config.php
@@ -0,0 +1,13 @@
+<?php
+
+$uri = get_template_directory_uri() . '/inc/admin/demo-data/demo-imgs/';
+// Demos
+$demos = array(
+	// Elementor Demos
+	'savour' => array(
+		'title'       => 'Savour',	
+		'description' => '',
+		'screenshot'  => $uri . 'savour.jpg',
+		'preview'     => 'https://demo.casethemes.net/savour/landing',
+	),
+);
